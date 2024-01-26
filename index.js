b765885706d5fc8ed9e6a30d64fccf494a597107
yugchauhan <yugchauhan715@gmail.com>
2024-01-26 02:40:49 -0800

    changes

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const multer = require("multer")
 const path = require("path")
 
 //Middleware
-app.use(cors({origin:'http://localhost:3000', credentials:true}))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 app.use(express.json())
 app.use(cookieParser())
 app.use("/images",express.static(path.join(__dirname,"/images")))
