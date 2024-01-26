e183af613a2884d5decb2b3de973fbe55e2edadc
yugchauhan <yugchauhan715@gmail.com>
2024-01-26 02:46:44 -0800

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path = require("path")
 
 //Middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://thebloger.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
