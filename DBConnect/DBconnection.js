79dd36e33663f97de108db6fe113b3f743e78558
yugchauhan <yugchauhan715@gmail.com>
2024-01-25 23:34:52 -0800

    changes

--- DBConnect/DBconnection.js
+++ DBConnect/DBconnection.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const MongoConnect=()=>{
-    try{
-        mongoose.connect('mongodb://localhost:27017/YugBlogs')
-        console.log("Database connected")
-    }
-    catch(error){
-        console.log("Error while connecting database -",error)
-    }
-    
-}
-module.exports=MongoConnect;
+const MongoConnect = async () => {
+  try {
+    await mongoose.connect(
+        process.env.DATABASE
+    );
+    console.log("Mongodb connected")
+  } catch (error) {
+    console.log("Error while connecting database -", error);
+  }
+};
+module.exports = MongoConnect;
