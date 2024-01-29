081bb6b41708904a57d666fb023e747c9ca7d63f
yugchauhan <yugchauhan715@gmail.com>
2024-01-29 00:14:36 -0800

    Changes

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ const multer = require("multer")
 const path = require("path")
 
 //Middleware
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://thebloger.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(cors({origin:"http://localhost:3000",credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/images",express.static(path.join(__dirname,"/images")))
